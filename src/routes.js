390caa4f111b1e37fd93674cb94992cdcf9d5581
Kiriyama8 <minumex8@gmail.com>
2020-03-25 02:24:10 -0300

    Adiciona a rota create de usuários no routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,8 @@
 const express = require('express');
 
+const crypto = require('crypto');
+const database = require('./database/database');
+
 const routes = express.Router();
 
 routes.get('/', (request, response) => {
@@ -7,6 +10,15 @@ routes.get('/', (request, response) => {
         hello_world: 'Hello World',
         data_atual: new Date()
     });
+});
+
+routes.post('/create', async (request, response) => {
+    const { nome, email } = request.body;
+    const id = crypto.randomBytes(6).toString('HEX');
+
+    await database('users').insert({ id, nome, email });
+
+    return response.json({ id });
 });
 
 module.exports = routes;
