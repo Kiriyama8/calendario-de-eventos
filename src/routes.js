bf51b00d04792a97bb3d8cd1998effe82b15c4ab
Kiriyama8 <minumex8@gmail.com>
2020-03-29 21:33:28 -0300

    Adiciona as novas rotas no arquivo routes

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,19 @@ const express = require('express');
 
 const UserController = require('./controller/UserController');
 const EventController = require('./controller/EventController');
+const SessionController = require('./controller/Auth/SessionController');
 
 const routes = express.Router();
 
-routes.get('/', EventController.index);
-routes.post('/users/store', UserController.create);
-routes.post('/events/store', EventController.create);
+routes.get('/eventos', EventController.index);
+
+routes.post('/sessions', SessionController.create);
+
+routes.get('/eventos/listar/:id', EventController.show);
+routes.post('/eventos/cadastrar', EventController.create);
+routes.delete('/eventos/deletar/:id', EventController.delete);
+
+routes.get('/usuarios/listar', UserController.index);
+routes.post('/usuarios/cadastrar', UserController.create);
 
 module.exports = routes;
