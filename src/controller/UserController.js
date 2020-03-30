d1125712a4e451c32c632d151f33ca79fff2ac55
Kiriyama8 <minumex8@gmail.com>
2020-03-29 21:31:35 -0300

    Adiciona o método para retornar a lista de todos os eventos cadastrados pelo usuário e remove a model User do controller

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,13 +1,36 @@
 const crypto = require('crypto');
-const usersDB = require('../models/User');
+const database = require('../database/database');
 
 module.exports = {
+
+    /**
+     * Retorna todos os eventos cadastrados pelo usuário
+     *
+     * @param request
+     * @param response
+     * @returns {Promise<Knex.ColumnBuilder|any>}
+     */
+    async index(request, response) {
+        const userId = request.headers.authorization;
+
+        const events = await database('events').where('user_id', userId).select();
+
+        return response.json(events);
+    },
+
+    /**
+     * Faz o cadastro de um novo usuário retornando o id gerado
+     *
+     * @param request
+     * @param response
+     * @returns {Promise<Knex.ColumnBuilder|any>}
+     */
     async create(request, response) {
         const { nome, email } = request.body;
         const id = crypto.randomBytes(6).toString('HEX');
-    
-        await usersDB.insert({ id, nome, email });
-    
+
+        await database('users').insert({ id, nome, email });
+
         return response.json({ id });
     }
 };
