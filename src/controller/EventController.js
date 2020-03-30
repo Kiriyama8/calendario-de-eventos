250fb5667ed472878d7496c069205713be069e3f
Kiriyama8 <minumex8@gmail.com>
2020-03-29 21:32:15 -0300

    Refatora o EventController por completo

--- src/controller/EventController.js
+++ src/controller/EventController.js
@@ -1,19 +1,87 @@
 const crypto = require('crypto');
-const eventDB = require('../models/Event');
+const database = require('../database/database');
 
 module.exports = {
+
+    /**
+     * Método responsável por retornar todos os eventos
+     *
+     * @param request
+     * @param response
+     * @returns {Promise<Knex.ColumnBuilder|any>}
+     */
     async index(request, response) {
-        const events = await eventDB.select('*');
+        const { page = 1 } = request.query;
+
+        const [count] = await database('events').count();
+
+        const events = await  database('events')
+            .join('users', 'users.id', '=', 'events.user_id')
+            .limit(5)
+            .offset((page - 1) * 5)
+            .select([
+                'events.*',
+                'users.nome',
+                'users.email'
+            ]);
+
+        response.header('X-Total-Count', count['count(*)']);
 
         return response.json(events);
     },
 
+    /**
+     * Método responsável por retornar um evento
+     *
+     * @param request
+     * @param response
+     * @returns {Promise<Knex.ColumnBuilder|any>}
+     */
+    async show(request, response) {
+        const id = request.body.id;
+
+        const event = await database('events').where('id', id).select();
+
+        return response.json({ event });
+    },
+
+    /**
+     * Método responsável por cadastrar um evento
+     *
+     * @param request
+     * @param response
+     * @returns {Promise<Knex.ColumnBuilder|any>}
+     */
     async create(request, response) {
-        const { titulo, descricao, horario, cidade, uf, user_id } = request.body;
+        const { titulo, descricao, horario, cidade, uf } = request.body;
         const id = crypto.randomBytes(6).toString('HEX');
-    
-        await eventDB.insert({ id, titulo, descricao, horario, cidade, uf, user_id });
-        
-        return response.json({ titulo, descricao, horario, cidade, uf });
+        const user_id = request.headers.authorization;
+
+        await database('events').insert({ id, titulo, descricao, horario, cidade, uf, user_id });
+
+        return response.json({ id });
+    },
+
+    /**
+     * Verifica se o usuário autenticado é o criador do evento a ser deletado
+     *
+     * @param request
+     * @param response
+     * @returns {Promise<Knex.ColumnBuilder|any|number|void>}
+     */
+    async delete(request, response) {
+        const { id } = request.body;
+        const user_id = request.headers.authorization;
+
+        const event = await database('events').where('id', id).select('user_id').first();
+
+        if (event.user_id != user_id) {
+            return response.status(401).json({ error: 'Operação não permitida!' });
+        }
+
+        await database('events').where('id', id).delete();
+
+        return response.status(204).send();
     }
+
 };
